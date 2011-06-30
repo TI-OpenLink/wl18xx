b7535f6dfdcdab3c6afdcb62b247b3f19bc9b905
Orit Brayer <orit@ti.com>
2011-06-30 15:35:19 +0300

    wl18xx: Do not send radio parameters to fw
    Signed-off-by: Orit Brayer <orit@ti.com>

--- drivers/net/wireless/wl12xx/init.h
+++ drivers/net/wireless/wl12xx/init.h
@@ -35,5 +35,6 @@ int wl1271_chip_specific_init(struct wl1271 *wl);
 int wl1271_hw_init(struct wl1271 *wl);
 int wl1271_init_ap_rates(struct wl1271 *wl);
 int wl1271_ap_init_templates(struct wl1271 *wl);
+int wl1271_radio_params_init(struct wl1271 *wl);
 
 #endif
