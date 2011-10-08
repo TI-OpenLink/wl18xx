4763140367e58609161df26e2dc3ba1eefab1e70
Luciano Coelho <coelho@ti.com>
2011-10-08 22:48:18 +0300

    wl12xx: add chip_family to platform data struct
    To abstract the bus modules from the actual driver that handles the
    chip, we can pass the chip_family struct in the platform data.  This
    string is used by the bus modules to name the platform device they
    create.  The same string is used by the platform driver to get probed.
    
    If not specified, we use "wl12xx" as default.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/sdio.c
+++ drivers/net/wireless/wl12xx/sdio.c
@@ -226,7 +226,11 @@ static int __devinit wl1271_probe(struct sdio_func *func,
 	/* Tell PM core that we don't need the card to be powered now */
 	pm_runtime_put_noidle(&func->dev);
 
-	glue->core = platform_device_alloc("wl12xx", -1);
+	/* get the chip family from pdata, if not available use default */
+	glue->core = platform_device_alloc(wlan_data->chip_family?
+					   wlan_data->chip_family:
+					   DEFAULT_CHIP_FAMILY,
+					   -1);
 	if (!glue->core) {
 		dev_err(glue->dev, "can't allocate platform_device");
 		ret = -ENOMEM;
