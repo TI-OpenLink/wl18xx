dca13d708a80c1280c9e21d57e6611b5a8a083b6
Pontus Fuchs <pontus.fuchs@gmail.com>
2011-10-24 11:15:42 +0200

    wl12xx: Add functions for managing rx data filters
    These functions will make sure filters are set according to what the
    firmware expects. It also stores the filter state in the driver.
    
    Signed-off-by: Pontus Fuchs <pontus.fuchs@gmail.com>

--- drivers/net/wireless/wl12xx/wl12xx.h
+++ drivers/net/wireless/wl12xx/wl12xx.h
@@ -413,6 +413,14 @@ enum rx_data_filter_action {
 	FILTER_FW_HANDLE = 2
 };
 
+struct wl12xx_rx_data_filter {
+	bool enabled;
+	enum rx_data_filter_action action;
+	u8 len;
+	u16 offset;
+	u8 pattern[WL1271_MAX_RX_DATA_FILTER_SIZE];
+};
+
 struct wl1271 {
 	struct platform_device *plat_dev;
 	struct ieee80211_hw *hw;
@@ -693,6 +701,16 @@ struct wl1271 {
 
 	/* AP-mode - work to add stations back on AP reconfig */
 	struct work_struct ap_start_work;
+
+	/* Global on/off switch for rx all rx filters */
+	bool rx_data_filter_enabled;
+
+	/* Default action for packets not matching any rule */
+	enum rx_data_filter_action rx_data_filter_policy;
+
+	/* RX Data filter rule descriptors */
+	struct wl12xx_rx_data_filter
+				rx_data_filters[WL1271_MAX_RX_DATA_FILTERS];
 };
 
 struct wl1271_station {
