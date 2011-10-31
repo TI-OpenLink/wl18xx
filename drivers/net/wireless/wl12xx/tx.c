8f1eb8afe97ed312d533775610201bc267eddc2b
Arik Nemtsov <arik@wizery.com>
2011-10-31 10:41:35 +0200

    18xx: queue tx-completions on the private wq
    bugfix - do not queue 18xx tx completions on the mac80211 wq.

--- drivers/net/wireless/wl12xx/tx.c
+++ drivers/net/wireless/wl12xx/tx.c
@@ -967,7 +967,7 @@ static void wl18xx_tx_complete_packet(struct wl1271 *wl, int tx_stat_byte)
 
 	/* return the packet to the stack */
 	skb_queue_tail(&wl->deferred_tx_queue, skb);
-	ieee80211_queue_work(wl->hw, &wl->netstack_work);
+	queue_work(wl->freezable_wq, &wl->netstack_work);
 	wl1271_free_tx_id(wl, id);
 }
 
