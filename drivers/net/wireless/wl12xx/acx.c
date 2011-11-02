72ebc5ddf5ae74fb96012d29dcff60ee63013b09
Lior Cohen <liorc@ti.com>
2011-11-02 12:06:46 +0200

    wl18xx: add rx BA window size configuration support using module param
    the default value is wl18xx_rx_aggr_size = 8.
    param should be sent to wl12xx.ko
    
    signed-off-by: Lior Cohen <liorc@ti.com>

--- drivers/net/wireless/wl12xx/acx.c
+++ drivers/net/wireless/wl12xx/acx.c
@@ -34,6 +34,8 @@
 #include "ps.h"
 #include "tx.h"
 
+extern u32 wl18xx_rx_aggr_size;
+
 int wl1271_acx_wake_up_conditions(struct wl1271 *wl,
 				  u8 wake_up_event, u8 listen_interval)
 {
@@ -1492,7 +1494,7 @@ int wl1271_acx_set_ba_receiver_session(struct wl1271 *wl, u8 tid_index,
 	acx->hlid = peer_hlid;
 	acx->tid = tid_index;
 	acx->enable = enable;
-	acx->win_size = RX_BA_WIN_SIZE;
+	acx->win_size = wl18xx_rx_aggr_size;
 	acx->ssn = ssn;
 
 	ret = wl1271_cmd_configure(wl, ACX_BA_SESSION_RX_SETUP, acx,
