73d1d7d1d9bc612c942c901a16544b582967d29c
Luciano Coelho <coelho@ti.com>
2011-11-02 17:13:28 +0200

    wlcore: read mailbox addresses
    Read the command and even mailbox addresses and save them in the
    context structure.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wlcore/boot.c
+++ drivers/net/wireless/ti/wlcore/boot.c
@@ -252,6 +252,17 @@ out:
 	return ret;
 }
 
+static void wlcore_prepare_mailbox(struct wlcore *wl)
+{
+	/* get hardware config command mail box */
+	wl->cmd_box_addr = wlcore_read_reg(wl, REG_COMMAND_MAILBOX_PTR);
+	/* get hardware config event mail box */
+	wl->event_box_addr = wlcore_read_reg(wl, REG_EVENT_MAILBOX_PTR);
+
+	wlcore_debug(DEBUG_BOOT, "cmd_box_addr 0x%x event_box_addr 0x%x",
+		     wl->cmd_box_addr, wl->event_box_addr);
+}
+
 int wlcore_boot(struct wlcore *wl)
 {
 	int ret;
@@ -300,6 +311,8 @@ int wlcore_boot(struct wlcore *wl)
 	ret = wlcore_boot_fw(wl);
 	if (ret < 0)
 		goto out_nvs;
+
+	wlcore_prepare_mailbox(wl);
 
 	goto out;
 
