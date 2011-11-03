25e2e0f4bdb1b693dc9dcc71680a513eb93dff9e
Orit Brayer <orit@ti.com>
2011-11-03 10:34:40 +0200

    wl18xx: change DRIVER_VERSION to "WL18XX_A1.12"
    Signed-off-by: Orit Brayer <orit@ti.com>

--- drivers/net/wireless/wl12xx/wl12xx.h
+++ drivers/net/wireless/wl12xx/wl12xx.h
@@ -40,7 +40,7 @@
 
 #define DRIVER_NAME "wl18xx"
 #define DRIVER_PREFIX DRIVER_NAME ": "
-#define DRIVER_VERSION "WL18XX_A1.12_Rx_Align"
+#define DRIVER_VERSION "WL18XX_A1.12"
 
 /*
  * FW versions support BA 11n
