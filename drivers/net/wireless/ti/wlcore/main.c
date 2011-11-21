b35454ebbbaadfd8553dfc95436b9e8d3a02c20b
Luciano Coelho <coelho@ti.com>
2011-11-21 20:37:14 +0200

    wlcore/wl12xx: implement chip-specific partition tables
    Add partition tables to wlcore, move and reorganize partition setting
    functions.  Move wl12xx partition table to use the wlcore partition
    table instead.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -1224,7 +1224,7 @@ static int wl1271_setup(struct wl1271 *wl)
 
 static int wl1271_chip_wakeup(struct wl1271 *wl)
 {
-	struct wl1271_partition_set partition;
+	struct wlcore_partition_set partition;
 	int ret = 0;
 
 	msleep(WL1271_PRE_POWER_ON_SLEEP);
@@ -1240,7 +1240,7 @@ static int wl1271_chip_wakeup(struct wl1271 *wl)
 	memset(&partition, 0, sizeof(partition));
 	partition.reg.start = REGISTERS_BASE;
 	partition.reg.size = REGISTERS_DOWN_SIZE;
-	wl1271_set_partition(wl, &partition);
+	wlcore_set_partition(wl, &partition);
 
 	/* ELP module wake up */
 	wl1271_fw_wakeup(wl);
@@ -5160,7 +5160,7 @@ int __devinit wlcore_probe(struct wl1271 *wl, struct platform_device *pdev)
 	unsigned long irqflags;
 	int ret;
 
-	if (!wl->ops) {
+	if (!wl->ops || !wl->ptable) {
 		ret = -EINVAL;
 		goto out_free_hw;
 	}
