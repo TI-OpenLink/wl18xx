cc728f875e729fa0aff0df2464d71ce4c80bbd0e
Luciano Coelho <coelho@ti.com>
2011-11-30 12:30:01 +0200

    wlcore/wl12xx: add quirk for legacy NVS support
    Instead of checking the chip ID directly in the wlcore code to decide
    whether to use the new or the old NVS format, we now use a quirk that
    should be set by the low level driver to say that it needs to use the
    old format.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wl12xx/main.c
+++ drivers/net/wireless/ti/wl12xx/main.c
@@ -146,7 +146,8 @@ static int wl12xx_identify_chip(struct wl1271 *wl)
 		wl1271_warning("chip id 0x%x (1271 PG10) support is obsolete",
 			       wl->chip.id);
 
-		wl->quirks |= WLCORE_QUIRK_NO_BLOCKSIZE_ALIGNMENT;
+		wl->quirks |= WLCORE_QUIRK_NO_BLOCKSIZE_ALIGNMENT |
+			WLCORE_QUIRK_LEGACY_NVS;
 		wl->fw_name = WL127X_FW_NAME;
 		break;
 
@@ -154,7 +155,8 @@ static int wl12xx_identify_chip(struct wl1271 *wl)
 		wl1271_debug(DEBUG_BOOT, "chip id 0x%x (1271 PG20)",
 			     wl->chip.id);
 
-		wl->quirks |= WLCORE_QUIRK_NO_BLOCKSIZE_ALIGNMENT;
+		wl->quirks |= WLCORE_QUIRK_NO_BLOCKSIZE_ALIGNMENT |
+			WLCORE_QUIRK_LEGACY_NVS;
 		wl->fw_name = WL127X_FW_NAME;
 		break;
 
