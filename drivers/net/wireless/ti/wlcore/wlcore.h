e57d8c1027d6005129a0720c6ee55378af1f553c
Arik Nemtsov <arik@wizery.com>
2011-12-05 22:33:50 +0200

    wlcore/wl18xx: enable MIMO/wide-chan rates in AP-mode rate config
    Add a HW op to add extra enabled rates for AP-mode data-rates. Since
    the rates might depend on channel properties, reconfigure AP-mode rates
    when these change.
    
    Implement the HW op for the 18xx family, where MIMO or wide-chan rates
    can be added.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- drivers/net/wireless/ti/wlcore/wlcore.h
+++ drivers/net/wireless/ti/wlcore/wlcore.h
@@ -64,6 +64,8 @@ struct wlcore_ops {
 	void (*set_rx_csum)(struct wl1271 *wl,
 			    struct wl1271_rx_descriptor *desc,
 			    struct sk_buff *skb);
+	u32 (*ap_get_mimo_wide_rate_mask)(struct wl1271 *wl,
+					  struct wl12xx_vif *wlvif);
 };
 
 enum wlcore_chip_family {
