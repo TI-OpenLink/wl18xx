e25843856b8ed69a7af91ff8b7534159aba13321
Orit Brayer <orit@ti.com>
2011-12-11 11:20:33 +0200

    wl18xx: Yield issue Work Around - part 5
    Use the existing retries mechanism for boot when the WA sequence
    fails on sdio read when trying to read the ocp state from the PHY PDSP.
    Increase the number of boot retries from 1 to 20.
    
    Signed-off-by: Orit Brayer <orit@ti.com>

--- drivers/net/wireless/wl12xx/wl12xx.h
+++ drivers/net/wireless/wl12xx/wl12xx.h
@@ -40,7 +40,7 @@
 
 #define DRIVER_NAME "wl18xx"
 #define DRIVER_PREFIX DRIVER_NAME ": "
-#define DRIVER_VERSION "WL18XX_A1.20"
+#define DRIVER_VERSION "WL18XX_A1.21"
 
 /*
  * FW versions support BA 11n
