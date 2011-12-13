ddc4c43eadeea5ffca86cf29982b787186e357c1
Arik Nemtsov <arik@wizery.com>
2011-12-13 16:54:20 +0200

    wl18xx: implement immediate Tx completion
    Implement immediate Tx completion for the 18xx family. Move 18xx
    specific Tx code to new tx.c/h files and create helper header files
    for definitions.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- drivers/net/wireless/ti/wl18xx/main.c
+++ drivers/net/wireless/ti/wl18xx/main.c
@@ -33,10 +33,8 @@
 
 #include "reg.h"
 #include "conf.h"
+#include "tx.h"
 
-#define WL18XX_TX_HW_BLOCK_SPARE        1
-#define WL18XX_TX_HW_GEM_BLOCK_SPARE    2
-#define WL18XX_TX_HW_BLOCK_SIZE         268
 
 static const u8 wl18xx_rate_to_idx_2ghz[] = {
 	/* MCS rates are used only with 11n */
@@ -469,6 +467,11 @@ static u32 wl18xx_get_rx_packet_len(struct wl1271 *wl, void *rx_data,
 	return data_len - sizeof(*desc);
 }
 
+static void wl18xx_tx_immediate_completion(struct wl1271 *wl)
+{
+	wl18xx_tx_immediate_complete(wl);
+}
+
 static struct wlcore_ops wl18xx_ops = {
 	.identify_chip	= wl18xx_identify_chip,
 	.boot		= wl18xx_boot,
@@ -480,6 +483,8 @@ static struct wlcore_ops wl18xx_ops = {
 	.get_rx_buf_align = wl18xx_get_rx_buf_align,
 	.read_data = wl18xx_read_data,
 	.get_rx_packet_len = wl18xx_get_rx_packet_len,
+	.tx_immediate_completion = wl18xx_tx_immediate_completion,
+	.tx_delayed_completion = NULL,
 };
 
 int __devinit wl18xx_probe(struct platform_device *pdev)
