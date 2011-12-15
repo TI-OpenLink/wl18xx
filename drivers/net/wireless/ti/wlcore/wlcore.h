9473e842c4cc155c702b06e3f9bd8c18867799d8
Luciano Coelho <coelho@ti.com>
2011-12-15 15:52:10 +0200

    wlcore/wl12xx: move ref_clock and tcxo_clock elements to wl12xx
    Only wl12xx needs to get the ref_clock anc tcxo_clock values from the
    platform data.  Move these elements from the wl1271 structure to
    wl12xx's private data.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wlcore/wlcore.h
+++ drivers/net/wireless/ti/wlcore/wlcore.h
@@ -127,7 +127,6 @@ struct wl1271 {
 
 	void (*set_power)(bool enable);
 	int irq;
-	int ref_clock;
 
 	spinlock_t wl_lock;
 
@@ -281,8 +280,6 @@ struct wl1271 {
 
 	/* bands supported by this instance of wl12xx */
 	struct ieee80211_supported_band bands[IEEE80211_NUM_BANDS];
-
-	int tcxo_clock;
 
 	/*
 	 * wowlan trigger was configured during suspend.
