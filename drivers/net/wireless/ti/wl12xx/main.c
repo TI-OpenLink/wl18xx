5629121e67305ab265c0befd866afecbd359430c
Eliad Peller <eliad@wizery.com>
2012-02-07 18:14:32 +0200

    wlcore/wl18xx/wl12xx: add version and timestamp strings (INTERNAL)
    In order to allow better version control, add version
    and timestamp strings into the wl12xx module.
    
    These strings will be printed on driver load, and when
    reading the driver_state.
    
    (this is not going to work well in compat, because it
    copies all the files instead of compiling them in the
    original tree, so the version of the compat tree will
    appear as the driver version. we can workaround it
    with the additional makefile hacks...)
    
    NOTE: I couldn't find how to make one module recompile
    when the .h file of another module was changed, so i
    just duplicated it for now.
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wl12xx/main.c
+++ drivers/net/wireless/ti/wl12xx/main.c
@@ -41,6 +41,7 @@
 #include "scan.h"
 #include "event.h"
 #include "debugfs.h"
+#include "version.h"
 
 static char *fref_param;
 static char *tcxo_param;
@@ -1774,6 +1775,7 @@ static int wl12xx_setup(struct wl1271 *wl)
 	if (!priv->rx_mem_addr)
 		return -ENOMEM;
 
+	wl1271_info("wl12xx driver version: %s", wl12xx_git_head);
 	return 0;
 }
 
--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -50,6 +50,7 @@
 #include "testmode.h"
 #include "scan.h"
 #include "hw_ops.h"
+#include "version.h"
 
 #define WL1271_BOOT_RETRIES 3
 
@@ -6152,6 +6153,9 @@ static void wlcore_nvs_cb(const struct firmware *fw, void *context)
 	}
 
 	wl->initialized = true;
+
+	wl1271_info("driver version: %s", wlcore_git_head);
+	wl1271_info("compilation time: %s", wlcore_timestamp);
 	goto out;
 
 out_hw_pg_ver:
