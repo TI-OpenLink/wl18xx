1dce25e5c7983d4e2e3857d2d8006e66d9b9ddab
Eliad Peller <eliad@wizery.com>
2012-03-18 13:54:38 +0200

    start making mac80211 support multi-channel

--- net/mac80211/tx.c
+++ net/mac80211/tx.c
@@ -1123,7 +1123,7 @@ ieee80211_tx_prepare(struct ieee80211_sub_if_data *sdata,
 	tx->skb = skb;
 	tx->local = local;
 	tx->sdata = sdata;
-	tx->channel = local->hw.conf.channel;
+	tx->channel = sdata->vif.bss_conf.channel;
 	__skb_queue_head_init(&tx->skbs);
 
 	/*
@@ -1394,7 +1394,7 @@ static bool ieee80211_tx(struct ieee80211_sub_if_data *sdata,
 		goto out;
 	}
 
-	tx.channel = local->hw.conf.channel;
+	tx.channel = sdata->vif.bss_conf.channel;
 	info->band = tx.channel->band;
 
 	/* set up hw_queue value early */
@@ -2294,7 +2294,7 @@ struct sk_buff *ieee80211_beacon_get_tim(struct ieee80211_hw *hw,
 	struct ieee80211_if_ap *ap = NULL;
 	struct beacon_data *beacon;
 	struct ieee80211_supported_band *sband;
-	enum ieee80211_band band = local->hw.conf.channel->band;
+	enum ieee80211_band band;
 	struct ieee80211_tx_rate_control txrc;
 
 	sband = local->hw.wiphy->bands[band];
@@ -2302,6 +2302,7 @@ struct sk_buff *ieee80211_beacon_get_tim(struct ieee80211_hw *hw,
 	rcu_read_lock();
 
 	sdata = vif_to_sdata(vif);
+	band = sdata->vif.bss_conf.channel;
 
 	if (!ieee80211_sdata_running(sdata))
 		goto out;
@@ -2701,7 +2702,7 @@ ieee80211_get_buffered_bc(struct ieee80211_hw *hw,
 	info = IEEE80211_SKB_CB(skb);
 
 	tx.flags |= IEEE80211_TX_PS_BUFFERED;
-	tx.channel = local->hw.conf.channel;
+	tx.channel = sdata->vif.bss_conf.channel;
 	info->band = tx.channel->band;
 
 	if (invoke_tx_handlers(&tx))
