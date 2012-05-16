15f5e33b9767d11e723b8a1ba2bf62289f34ff35
Victor Goldenshtein <victorg@ti.com>
2012-05-16 11:58:24 +0300

    nl80211/cfg80211: add new scan configurations attributes (INTERNAL)
    To enhance scan abilities by being able to setup some low level
    scan parameters dynamically during NL80211_CMD_TRIGGER_SCAN
    command.
    
    Exposing 3 new attributes:
      NL80211_ATTR_SCAN_MIN_DWELL: Minimum scan dwell time (in TUs), u32
    	attribute to setup minimum time to wait on each channel, if received
    	at least one probe response during this period will continue waiting
    	%NL80211_ATTR_SCAN_MAX_DWELL, otherwise will move to next channel.
     	This is optional attribute, so if it's not set driver should
    	use hardware default values.
      NL80211_ATTR_SCAN_MAX_DWELL: Maximum scan dwell time (in TUs), u32
    	attribute to setup maximum time to wait on each channel.
    	This is optional attribute, so if it's not set driver should
    	use hardware default values.
      NL80211_ATTR_SCAN_NUM_PROBE:  Attribute (u8) to setup number of probe
     	requests to transmit on each active scan channel, used with
    	NL80211_CMD_TRIGGER_SCAN command.
    
    These attributes are also required for the beacon reports in the
    802.11k standard.
    
    Signed-off-by: Victor Goldenshtein <victorg@ti.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -1001,6 +1001,9 @@ struct cfg80211_ssid {
  * @wiphy: the wiphy this was for
  * @wdev: the wireless device to scan for
  * @aborted: (internal) scan request was notified as aborted
+ * @min_dwell: minimum time to wait on each channel for active scans
+ * @max_dwell: maximum time to wait on each channel for active scans
+ * @num_probe: number of probe requests to transmit on each active scan channel
  * @no_cck: used to send probe requests at non CCK rate in 2GHz band
  */
 struct cfg80211_scan_request {
@@ -1018,6 +1021,10 @@ struct cfg80211_scan_request {
 	struct wiphy *wiphy;
 	bool aborted;
 	bool no_cck;
+
+	u32 min_dwell;
+	u32 max_dwell;
+	u8 num_probe;
 
 	/* keep last */
 	struct ieee80211_channel *channels[0];
