303f156a5d5762b554c23dab98a334a05bc463ea
Victor Goldenshtein <victorg@ti.com>
2012-05-16 11:58:24 +0300

    nl80211/cfg80211: add new scan configurations attributes (INTERNAL)
    To enhance scan abilities by being able to setup some low level
    scan parameters dynamically during NL80211_CMD_TRIGGER_SCAN
    command.
    
    Exposing 3 new attributes:
      NL80211_ATTR_SCAN_MIN_DWELL: Minimum scan dwell time (in TUs), u32
    	attribute to setup minimum time to wait on each channel, if received
    	at least one probe response during this period will continue waiting
    	%NL80211_ATTR_SCAN_MAX_DWELL, otherwise will move to next channel.
     	This is optional attribute, so if it's not set driver should
    	use hardware default values.
      NL80211_ATTR_SCAN_MAX_DWELL: Maximum scan dwell time (in TUs), u32
    	attribute to setup maximum time to wait on each channel.
    	This is optional attribute, so if it's not set driver should
    	use hardware default values.
      NL80211_ATTR_SCAN_NUM_PROBE:  Attribute (u8) to setup number of probe
     	requests to transmit on each active scan channel, used with
    	NL80211_CMD_TRIGGER_SCAN command.
    
    These attributes are also required for the beacon reports in the
    802.11k standard.
    
    Signed-off-by: Victor Goldenshtein <victorg@ti.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -1531,6 +1531,21 @@ enum nl80211_commands {
  *
  * @NL80211_ATTR_RXMGMT_FLAGS: flags for nl80211_send_mgmt(), u32.
  *	As specified in the &enum nl80211_rxmgmt_flags.
+ * @%NL80211_ATTR_SCAN_MIN_DWELL: Minimum scan dwell time (in TUs), u32
+ *	attribute to setup minimum time to wait on each channel, if received
+ *	at least one probe response during this period will continue waiting
+ *	%NL80211_ATTR_SCAN_MAX_DWELL, otherwise will move to next channel.
+ *	Relevant only for active scan, used with %NL80211_CMD_TRIGGER_SCAN
+ *	command. This is optional attribute, so if it's not set driver should
+ *	use hardware default values.
+ * @%NL80211_ATTR_SCAN_MAX_DWELL: Maximum scan dwell time (in TUs), u32
+ *	attribute to setup maximum time to wait on each channel.
+ *	Relevant only for active scan, used with %NL80211_CMD_TRIGGER_SCAN
+ *	command. This is optional attribute, so if it's not set driver should
+ *	use hardware default values.
+ * @%NL80211_ATTR_SCAN_NUM_PROBE:  Attribute (u8) to setup number of probe
+ *	requests to transmit on each active scan channel, used with
+ *	%NL80211_CMD_TRIGGER_SCAN command.
  *
  * @NL80211_ATTR_STA_SUPPORTED_CHANNELS: array of supported channels.
  *
@@ -1913,6 +1928,10 @@ enum nl80211_attrs {
 	NL80211_ATTR_MAX_AP_ASSOC_STA,
 
 	NL80211_ATTR_CH_SWITCH_IFACES,
+
+	NL80211_ATTR_SCAN_MIN_DWELL,
+	NL80211_ATTR_SCAN_MAX_DWELL,
+	NL80211_ATTR_SCAN_NUM_PROBE,
 
 	/* add attributes here, update the policy in nl80211.c */
 
