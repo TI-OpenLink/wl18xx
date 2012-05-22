c81a45ca55c1b761280d242553388aeea7f30ff6
Ido Reis <idor@ti.com>
2012-05-22 12:41:36 +0300

    wl18xx: enable fw logger for pg2 chips
    fw logger is avialble for pg2 devices from fw 8.2.0.0.91
    
    Signed-off-by: Ido Reis <idor@ti.com>

--- drivers/net/wireless/ti/wl18xx/main.c
+++ drivers/net/wireless/ti/wl18xx/main.c
@@ -628,7 +628,6 @@ static int wl18xx_identify_chip(struct wl1271 *wl)
 		/* wl18xx uses the same firmware for PLT */
 		wl->plt_fw_name = WL18XX_FW_PG2_NAME;
 		wl->quirks |= WLCORE_QUIRK_NO_ELP |
-				  WLCORE_QUIRK_FWLOG_NOT_IMPLEMENTED |
 				  WLCORE_QUIRK_RX_BLOCKSIZE_ALIGN;
 		if (enable_pad_last_frame) {
 			wl->quirks |= WLCORE_QUIRK_TX_PAD_LAST_FRAME;
