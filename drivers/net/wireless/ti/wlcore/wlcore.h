ca201889eeb8063ed0f46e28620433069f163d3e
Yoni Divinsky <yoni.divinsky@ti.com>
2012-06-14 12:29:16 +0300

    wlcore: add probe request templates for sched and one-shot scans
    The driver configures the firmware template for probe requests during
    the scan process.  If the same template is used for one-shot and sched
    scans they will override each other when running scans simultaneously.
    
    This fix works only on firmwares later than
    X.3.9.2.112 for single role and
    X.3.9.2.23  for multi-role
    
    Signed-off-by: Yoni Divinsky <yoni.divinsky@ti.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wlcore/wlcore.h
+++ drivers/net/wireless/ti/wlcore/wlcore.h
@@ -448,6 +448,9 @@ int wlcore_set_key(struct wl1271 *wl, enum set_key_cmd cmd,
 /* Some firmwares not support sched scans while connected */
 #define WLCORE_QUIRK_NO_SCHED_SCAN_WHILE_CONN	BIT(9)
 
+/* separate probe response templates for one-shot and sched scans */
+#define WLCORE_QUIRK_SEPARATED_PROBE_TEMPLATES	BIT(10)
+
 /* TODO: move to the lower drivers when all usages are abstracted */
 #define CHIP_ID_1271_PG10              (0x4030101)
 #define CHIP_ID_1271_PG20              (0x4030111)
