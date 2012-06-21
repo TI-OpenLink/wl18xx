60ed71546e788cc74bf2aaba4a93d06884a3ee0e
Eliad Peller <eliad@wizery.com>
2012-06-21 10:08:15 +0300

    wlcore: update channel_type on BSS_CHANGED_CHANNEL
    we didn't update the channel_type BSS_CHANGED_CHANNEL,
    making start_sta always use NO_HT channel

--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -4069,6 +4069,7 @@ static void wl1271_op_bss_info_changed(struct ieee80211_hw *hw,
 		wl1271_tx_work_locked(wl);
 		wlvif->band = bss_conf->channel->band;
 		wlvif->channel = channel;
+		wlvif->channel_type = bss_conf->channel_type;
 	}
 
 	if (unlikely(wl->state == WL1271_STATE_OFF))
