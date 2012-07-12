80ddd9586789462089f9d29c6e8f437c46625eba
Ido Reis <idor@ti.com>
2012-07-12 21:22:59 +0300

    wl18xx: set default phy config to hp siso
    update phy power values and ht mode for hp siso board.
    
    Signed-off-by: Ido Reis <idor@ti.com>

--- drivers/net/wireless/ti/wl18xx/main.c
+++ drivers/net/wireless/ti/wl18xx/main.c
@@ -497,7 +497,7 @@ static struct wlcore_conf wl18xx_conf = {
 
 static struct wl18xx_priv_conf wl18xx_default_priv_conf = {
 	.ht = {
-		.mode				= HT_MODE_MIMO,
+		.mode				= HT_MODE_WIDE,
 	},
 	.phy = {
 		.phy_standalone			= 0x00,
@@ -523,8 +523,8 @@ static struct wl18xx_priv_conf wl18xx_default_priv_conf = {
 		.rx_profile			= 0x00,
 		.pwr_limit_reference_11_abg	= 0xc8,
 		.Psat				= 0,
-		.low_power_val			= 0x00,
-		.med_power_val			= 0x0a,
+		.low_power_val			= 0x05,
+		.med_power_val			= 0x14,
 		.high_power_val			= 0x1e,
 		.external_pa_dc2dc		= 0,
 		.number_of_assembled_ant2_4	= 1,
