e338ff1e4a9497de15d64b82e7f1671222e67c46
Igal Chernobelsky <igalc@ti.com>
2012-07-31 14:43:55 +0300

    wlcore: add num tx desc parameter to alloc_hw
    Add number of Tx descriptors as new paramter to wlcore_alloc_hw function.
    
    Signed-off-by: Igal Chernobelsky <igalc@ti.com>

--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -5902,7 +5902,7 @@ static int wl1271_init_ieee80211(struct wl1271 *wl)
 }
 
 struct ieee80211_hw *wlcore_alloc_hw(size_t priv_size, u32 aggr_buf_size,
-				     u32 mbox_size)
+				     u32 mbox_size, u32 num_tx_desc)
 {
 	struct ieee80211_hw *hw;
 	struct wl1271 *wl;
