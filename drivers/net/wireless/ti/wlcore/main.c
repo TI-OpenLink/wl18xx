4e7c2725d4c1864d3db7f2513d5580c7973a1181
Arik Nemtsov <arik@wizery.com>
2012-08-05 20:02:10 +0300

    wlcore: block most mac80211 ops on p2p mgmt iface (INTERNAL)
    In general, p2p management interfaces cannot send vif-specific commands
    to FW, as the vif role id is invalid. Only off-channel data and scans
    happen on this vif, so most ops are not relevant.
    
    If the vif is a p2p mgmt vif, block non relevant mac80211 ops.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -1806,6 +1806,9 @@ static int wl1271_op_suspend(struct ieee80211_hw *hw,
 	mutex_lock(&wl->mutex);
 	wl->wow_enabled = true;
 	wl12xx_for_each_wlvif(wl, wlvif) {
+		if (wl12xx_wlvif_to_vif(wlvif)->dummy_p2p)
+			continue;
+
 		ret = wl1271_configure_suspend(wl, wlvif, wow);
 		if (ret < 0) {
 			mutex_unlock(&wl->mutex);
@@ -1891,6 +1894,9 @@ static int wl1271_op_resume(struct ieee80211_hw *hw)
 	}
 
 	wl12xx_for_each_wlvif(wl, wlvif) {
+		if (wl12xx_wlvif_to_vif(wlvif)->dummy_p2p)
+			continue;
+
 		wl1271_configure_resume(wl, wlvif);
 	}
 
@@ -3077,6 +3083,9 @@ static int wl12xx_config_vif(struct wl1271 *wl, struct wl12xx_vif *wlvif,
 {
 	int ret;
 
+	if (wl12xx_wlvif_to_vif(wlvif)->dummy_p2p)
+		return 0;
+
 	if (conf->power_level != wlvif->power_level) {
 		ret = wl1271_acx_tx_power(wl, wlvif, conf->power_level);
 		if (ret < 0)
@@ -3197,6 +3206,9 @@ static void wl1271_op_configure_filter(struct ieee80211_hw *hw,
 		goto out;
 
 	wl12xx_for_each_wlvif(wl, wlvif) {
+		if (wl12xx_wlvif_to_vif(wlvif)->dummy_p2p)
+			continue;
+
 		if (wlvif->bss_type != BSS_TYPE_AP_BSS) {
 			if (*total & FIF_ALLMULTI)
 				ret = wl1271_acx_group_address_tbl(wl, wlvif,
@@ -3457,6 +3469,9 @@ int wlcore_set_key(struct wl1271 *wl, enum set_key_cmd cmd,
 
 	wl1271_debug(DEBUG_MAC80211, "mac80211 set key");
 
+	if (vif->dummy_p2p)
+		return 0;
+
 	wl1271_debug(DEBUG_CRYPT, "CMD: 0x%x sta: %p", cmd, sta);
 	wl1271_debug(DEBUG_CRYPT, "Key: algo:0x%x, id:%d, len:%d flags 0x%x",
 		     key_conf->cipher, key_conf->keyidx,
@@ -3560,6 +3575,9 @@ static void wl1271_op_set_default_key_idx(struct ieee80211_hw *hw,
 
 	wl1271_debug(DEBUG_MAC80211, "mac80211 set default key idx %d",
 		     key_idx);
+
+	if (vif->dummy_p2p)
+		return;
 
 	mutex_lock(&wl->mutex);
 
@@ -3723,6 +3741,9 @@ static int wl1271_op_sched_scan_start(struct ieee80211_hw *hw,
 
 	wl1271_debug(DEBUG_MAC80211, "wl1271_op_sched_scan_start");
 
+	if (vif->dummy_p2p)
+		return -EINVAL;
+
 	if (req->n_short_intervals > SCAN_MAX_SHORT_INTERVALS) {
 		wl1271_warning("Number of short intervals requested (%d)"
 			       "exceeds limit (%d)", req->n_short_intervals,
@@ -3762,6 +3783,9 @@ static void wl1271_op_sched_scan_stop(struct ieee80211_hw *hw,
 	int ret;
 
 	wl1271_debug(DEBUG_MAC80211, "wl1271_op_sched_scan_stop");
+
+	if (vif->dummy_p2p)
+		return;
 
 	mutex_lock(&wl->mutex);
 
@@ -4539,6 +4563,9 @@ static void wl1271_op_bss_info_changed(struct ieee80211_hw *hw,
 	wl1271_debug(DEBUG_MAC80211, "mac80211 bss info role %d changed 0x%x",
 		     wlvif->role_id, (int)changed);
 
+	if (vif->dummy_p2p)
+		return;
+
 	/*
 	 * make sure to cancel pending disconnections if our association
 	 * state changed
@@ -4662,6 +4689,9 @@ static int wl1271_op_conf_tx(struct ieee80211_hw *hw,
 	u8 ps_scheme;
 	int ret = 0;
 
+	if (vif->dummy_p2p)
+		return 0;
+
 	mutex_lock(&wl->mutex);
 
 	wl1271_debug(DEBUG_MAC80211, "mac80211 conf tx %d", queue);
@@ -4713,6 +4743,9 @@ static u64 wl1271_op_get_tsf(struct ieee80211_hw *hw,
 	int ret;
 
 	wl1271_debug(DEBUG_MAC80211, "mac80211 get tsf");
+
+	if (vif->dummy_p2p)
+		return mactime;
 
 	mutex_lock(&wl->mutex);
 
@@ -5022,6 +5055,9 @@ static int wl12xx_op_sta_state(struct ieee80211_hw *hw,
 	wl1271_debug(DEBUG_MAC80211, "mac80211 sta %d state=%d->%d",
 		     sta->aid, old_state, new_state);
 
+	if (vif->dummy_p2p)
+		return 0;
+
 	mutex_lock(&wl->mutex);
 
 	if (unlikely(wl->state != WLCORE_STATE_ON)) {
@@ -5099,6 +5135,9 @@ static int wl1271_op_ampdu_action(struct ieee80211_hw *hw,
 
 	wl1271_debug(DEBUG_MAC80211, "mac80211 ampdu action %d tid %d", action,
 		     tid);
+
+	if (vif->dummy_p2p)
+		return 0;
 
 	/* sanity check - the fields in FW are only 8bits wide */
 	if (WARN_ON(tid > 0xFF))
@@ -5291,6 +5330,9 @@ static void wl12xx_op_channel_switch(struct ieee80211_hw *hw,
 	wl12xx_for_each_wlvif_sta(wl, wlvif) {
 		unsigned long delay_usec;
 
+		if (wl12xx_wlvif_to_vif(wlvif)->dummy_p2p)
+			continue;
+
 		if (!test_bit(WLVIF_FLAG_STA_ASSOCIATED, &wlvif->flags))
 			continue;
 
@@ -5473,6 +5515,9 @@ static int wlcore_op_get_rssi(struct ieee80211_hw *hw,
 	int ret = 0;
 
 	wl1271_debug(DEBUG_MAC80211, "mac80211 get_rssi");
+
+	if (vif->dummy_p2p)
+		return -EINVAL;
 
 	mutex_lock(&wl->mutex);
 
