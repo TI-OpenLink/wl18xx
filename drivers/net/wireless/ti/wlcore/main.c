2cb96b346d029c4b1f13dfd362573cc0078c921f
Eliad Peller <eliad@wizery.com>
2012-09-03 17:45:29 +0300

    wlcore: check wl->state before setting default key
    wl1271_op_set_default_key_idx() lacks a check
    of wl->state, causing the driver to try writing
    to a powered-off device in some cases (e.g.
    during recovery)
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>

--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -3171,6 +3171,11 @@ static int wl1271_op_set_default_key_idx(struct ieee80211_hw *hw,
 
 	mutex_lock(&wl->mutex);
 
+	if (unlikely(wl->state != WLCORE_STATE_ON)) {
+		ret = -EAGAIN;
+		goto out_unlock;
+	}
+
 	ret = wl1271_ps_elp_wakeup(wl);
 	if (ret < 0)
 		goto out_unlock;
