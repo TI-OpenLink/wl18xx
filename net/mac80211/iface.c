330a35c76354d2c640f28feace1870b36fa1194d
Victor Goldenshtein <victorg@ti.com>
2012-12-26 10:16:26 +0200

    mac80211: fix delayed ADDBA response
    Block frame processing during scan might delay the
    ADDBA response, which eventually timeouts and
    significantly reduces the device throughput.
    Remove this constrain as it's not required for the
    HW scan.
    
    Signed-off-by: Victor Goldenshtein <victorg@ti.com>

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -1128,7 +1128,7 @@ static void ieee80211_iface_work(struct work_struct *work)
 	if (!ieee80211_sdata_running(sdata))
 		return;
 
-	if (local->scanning)
+	if (local->scanning && !local->ops->hw_scan)
 		return;
 
 	/*
