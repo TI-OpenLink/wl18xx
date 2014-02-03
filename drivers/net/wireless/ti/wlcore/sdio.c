d593f9abc178399507e3ad0d43eba945f955018b
Yaniv Machani <yanivma@ti.com>
2014-02-03 15:56:51 +0200

    wl12xx : fix platform data struct to match old kernels (INTERNAL)
    This patch should be used only in case the kernel can't be compiled with the gpio alignment.
    It is relevant mostly when using pre-compiled kernel with unaligned struct (kernel 3.2)
    in any other cases this should not be used.
    
    Signed-off-by: Yaniv Machani <yanivma@ti.com>

--- drivers/net/wireless/ti/wlcore/sdio.c
+++ drivers/net/wireless/ti/wlcore/sdio.c
@@ -227,7 +227,6 @@ static struct wl12xx_platform_data *get_platform_data(struct device *dev)
 {
 	struct wl12xx_platform_data *pdata;
 	struct device_node *np;
-	u32 gpio;
 
 	pdata = wl12xx_get_platform_data();
 	if (!IS_ERR(pdata))
@@ -246,13 +245,7 @@ static struct wl12xx_platform_data *get_platform_data(struct device *dev)
 		return NULL;
 	}
 
-	if (of_property_read_u32(np, "irq", &pdata->irq)) {
-		if (!of_property_read_u32(np, "gpio", &gpio) &&
-		    !gpio_request_one(gpio, GPIOF_IN, "wlcore_irq")) {
-			pdata->gpio = gpio;
-			pdata->irq = gpio_to_irq(gpio);
-		}
-	}
+	of_property_read_u32(np, "irq", &pdata->irq);
 
 	/* Optional fields */
 	pdata->use_eeprom = of_property_read_bool(np, "use-eeprom");
@@ -269,9 +262,6 @@ static struct wl12xx_platform_data *get_platform_data(struct device *dev)
 
 static void del_platform_data(struct wl12xx_platform_data *pdata)
 {
-	if (pdata->gpio)
-		gpio_free(pdata->gpio);
-
 	kfree(pdata);
 }
 
